2985c074cfbce2747242b85d079b6b6a18723557
Sammidysam <sammidysam@gmail.com>
2015-07-28 11:43:04 -0400

    Add SessionController code
    To no surprise, also stolen from intranet2015

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,10 +1,21 @@
 class SessionController < ApplicationController
-  def new
-  end
+	def new
+	end
 
-  def create
-  end
+	def create
+		if u = User.authenticate(params[:username], params[:password])
+			session[:udn] = u.dn.to_s
+			flash[:notice] = "Welcome, #{u.nice_name}"
+		else
+			flash[:error] = "Login failed."
+		end
 
-  def destroy
-  end
+		redirect_to root_path
+	end
+
+	def destroy
+		session.delete :udn
+
+		redirect_to root_path
+	end
 end
