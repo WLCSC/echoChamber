9806db086fb2c5a4a5b6c65ea3a2e91ad5807bcd
Sammidysam <sammidysam@gmail.com>
2015-07-28 13:10:41 -0400

    Make APP_CONFIG have indifferent access

--- config/initializers/ldap.rb
+++ config/initializers/ldap.rb
@@ -1,18 +1,18 @@
-APP_CONFIG = YAML.load_file(Rails.root.to_s + ('/config/app.yml'))[Rails.env]
+APP_CONFIG = YAML.load_file(Rails.root.to_s + ("/config/app.yml"))[Rails.env].with_indifferent_access
 
 unless APP_CONFIG[:ignore_ldap] == true
-  require 'active_ldap'
+	require "active_ldap"
 
-  LDAP_CONFIG = YAML.load_file(Rails.root.to_s + ('/config/ldap.yml'))[Rails.env]
-  ActiveLdap::Base.setup_connection LDAP_CONFIG
+	LDAP_CONFIG = YAML.load_file(Rails.root.to_s + ("/config/ldap.yml"))[Rails.env]
+	ActiveLdap::Base.setup_connection LDAP_CONFIG
 
-  class ActiveLdap::Base
-    def to_s
-      dn.to_s
-    end
+	class ActiveLdap::Base
+		def to_s
+			dn.to_s
+		end
 
-    def inspect
-      "LDAP[#{to_s}]"
-    end
-  end
+		def inspect
+			"LDAP[#{to_s}]"
+		end
+	end
 end
