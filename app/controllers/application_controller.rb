9ab949b6370714eb5a06b806d9a1fae4a1ea1b9e
Sammidysam <sammidysam@gmail.com>
2015-07-28 10:44:43 -0400

    Add dummy user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+	# Prevent CSRF attacks by raising an exception.
+	# For APIs, you may want to use :null_session instead.
+	protect_from_forgery with: :exception
+
+	if APP_CONFIG[:ignore_ldap]
+		@current_user ||= DummyUser.new
+	else
+		@current_user ||= (session[:udn] ? User.find(session[:udn]) : nil)
+	end
 end
---
+++ app/models/dummy.rb
@@ -0,0 +1,9 @@
+class DummyUser
+	def name
+		"dummy"
+	end
+
+	def sid
+		"dummy"
+	end
+end
