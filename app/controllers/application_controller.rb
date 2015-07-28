6a26ab4063f371758869d62a7cb3f045a99ca605
Sammidysam <sammidysam@gmail.com>
2015-07-28 10:50:53 -0400

    Steal more methods from intranet2015

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,26 @@ class ApplicationController < ActionController::Base
 	# For APIs, you may want to use :null_session instead.
 	protect_from_forgery with: :exception
 
-	if APP_CONFIG[:ignore_ldap]
-		@current_user ||= DummyUser.new
-	else
-		@current_user ||= (session[:udn] ? User.find(session[:udn]) : nil)
+	def current_user
+		if APP_CONFIG[:ignore_ldap]
+			@current_user ||= DummyUser.new
+		else
+			@current_user ||= (session[:udn] ? User.find(session[:udn]) : nil)
+		end
+	end
+
+	def current_admin
+		@current_admin ||= (current_user && current_user.admin?)
+	end
+
+	helper_method :current_user, :current_admin
+
+	private
+	def check_for_user
+		rediect_to root_path, alert: "Please log in." unless APP_CONFIG[:ignore_ldap] || current_user
+	end
+
+	def check_for_admin
+		redirect_to root_path, alert: "You can't do that." unless APP_CONFIG[:ignore_ldap] || current_user
 	end
 end
