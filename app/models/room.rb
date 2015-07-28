b775ac85fe261a48fc4df16e5eff625c625cf7f7
Sammidysam <sammidysam@gmail.com>
2015-07-28 11:21:15 -0400

    Fix up some relations

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,3 @@
 class Room < ActiveRecord::Base
-  belongs_to :last_file_id
+	belongs_to :last_file_id, class_name: "Sound"
 end
--- app/models/sound.rb
+++ app/models/sound.rb
@@ -1,2 +1,5 @@
 class Sound < ActiveRecord::Base
+	has_attached_file :data
+
+	validates :data, attachment_content_type: "audio/mpeg"
 end
