6c3d6ce0f56e9c771409cc099916422f0ff12a9c
Sammidysam <sammidysam@gmail.com>
2015-07-31 14:30:18 -0400

    Display what file will be playing

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,5 @@
 class Room < ActiveRecord::Base
-	belongs_to :last_file_id, class_name: "Sound"
+	belongs_to :last_sound, class_name: "Sound"
 
 	has_many :sounds
 	validates :name, :user_sid, :password, presence: true
