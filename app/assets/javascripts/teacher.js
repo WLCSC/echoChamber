1156e68cc22e1e6e1bf0deba0c47c271732b63d0
Sammidysam <sammidysam@gmail.com>
2015-08-03 14:00:06 -0400

    Play and Pause buttons working

--- app/assets/javascripts/teacher.js
+++ app/assets/javascripts/teacher.js
@@ -1,1 +1,11 @@
 var teacherClient = new Faye.Client("http://localhost:3000/faye");
+
+play = function (path) {
+	document.getElementById("jukebox").play();
+	$("#logo").attr("src", path);
+};
+
+pause = function (path) {
+	document.getElementById("jukebox").pause();
+	$("#logo").attr("src", path);
+};
