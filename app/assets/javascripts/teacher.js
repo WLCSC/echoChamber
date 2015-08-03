81ece24f46d47f7f830d0e867d3308c519bcfdf1
Sammidysam <sammidysam@gmail.com>
2015-08-03 16:35:40 -0400

    Revert "Get slider value in a better way"
    This reverts commit d0733d6bb7d049b0b67769503dd99bb08aa55ad9.

--- app/assets/javascripts/teacher.js
+++ app/assets/javascripts/teacher.js
@@ -17,8 +17,8 @@ setUpSlider = function (room_id) {
 		min: 0,
 		//max: document.getElementById("jukebox").duration,
 		range: "min",
-		slide: function (event, ui) {
-			var value = ui.value;
+		change: function () {
+			var value = $("#slider").slider("value");
 			document.getElementById("jukebox").fastSeek(value);
 			teacherClient.publish("/echochamber/" + room_id, { seek: value });
 		}
